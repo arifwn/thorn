9bf6803dd68d3917bdbebf01819c45075741de18
Arif Widi Nugroho <arif@sainsmograf.com>
2012-06-09 23:00:41 +0700

    change input format to <winddir> <windspeed> <frequency>

--- thorn.py
+++ thorn.py
@@ -15,7 +15,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 import numpy as np
-from numpy.random import random
+from numpy.random import random, random_integers
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
@@ -46,11 +46,17 @@ class Qt4MplCanvas(FigureCanvas):
         # notify the system of updated policy
         FigureCanvas.updateGeometry(self)
     
-    def plot(self, windir, windspeed):
-        self.windir = windir
-        self.winspeed = windspeed
+    def plot(self, windir, windspeed, windfrequency):
+        self.windir = []
+        self.windspeed = []
+        
+        for i, frequency in enumerate(windfrequency):
+            for n in range(frequency):
+                self.windir.append(windir[i])
+                self.windspeed.append(windspeed[i])
+        
         self.axes.clear()
-        self.axes.bar(windir, windspeed, normed=True, opening=0.8, edgecolor='white')
+        self.axes.bar(self.windir, self.windspeed, normed=True, opening=0.8, edgecolor='white')
         l = self.axes.legend(borderaxespad=-3.8)
         setp(l.get_texts(), fontsize=8)
         
@@ -82,10 +88,11 @@ class MainWindow(QMainWindow):
         # create sample plot
         windir = random(10)*360
         windspeed = random(10)*6
-        self.mplcanvas.plot(windir, windspeed)
+        windfrequency = random_integers(1, 25, 10)
+        self.mplcanvas.plot(windir, windspeed, windfrequency)
         
         # put sample data in the text editor
-        self.data_editor.setPlainText(construct_text_data(windir, windspeed))
+        self.data_editor.setPlainText(construct_text_data(windir, windspeed, windfrequency))
         
         # pack these widget into the vertical box
         vbl.addWidget(self.mplcanvas, 0, 0)
@@ -102,18 +109,18 @@ class MainWindow(QMainWindow):
         self.connect(self.render_button, SIGNAL('clicked()'), self.render)
     
     def render(self):
-        windir, windspeed = parse_text_data(str(self.data_editor.toPlainText()))
-        self.mplcanvas.plot(windir, windspeed)
+        windir, windspeed, frequency = parse_text_data(str(self.data_editor.toPlainText()))
+        self.mplcanvas.plot(windir, windspeed, frequency)
 
 
-def construct_text_data(windir, windspeed):
+def construct_text_data(windir, windspeed, windfrequency):
     buff = StringIO()
-    buff.write('# windir windspeed\n')
+    buff.write('# windir windspeed frequency\n')
     
-    wind_data = zip(windir, windspeed)
+    wind_data = zip(windir, windspeed, windfrequency)
     
-    for wd, ws in wind_data:
-        buff.write('%.2f %.2f\n' % (wd, ws))
+    for direction, speed, frequency in wind_data:
+        buff.write('%.2f %.2f %d\n' % (direction, speed, frequency))
 
     return buff.getvalue()
 
@@ -122,6 +129,7 @@ def parse_text_data(data):
     lines = data.splitlines()
     windir = []
     windspeed = []
+    windfrequency = []
     
     for line in lines:
         line_ = line.strip()
@@ -130,18 +138,20 @@ def parse_text_data(data):
         if line[0] == '#':
             continue
         wind = line.split()
-        if len(wind) != 2:
+        if len(wind) != 3:
             continue
         try:
-            wd = float(wind[0])
-            ws = float(wind[1])
+            direction = float(wind[0])
+            speed = float(wind[1])
+            frequency = int(wind[2])
         except ValueError:
             continue
         
-        windir.append(wd)
-        windspeed.append(ws)
+        windir.append(direction)
+        windspeed.append(speed)
+        windfrequency.append(frequency)
     
-    return windir, windspeed
+    return windir, windspeed, windfrequency
 
 
 if __name__ == '__main__':
